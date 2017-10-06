33e1a2d78c340fc9dd7a82f36e938c5e860597f2
Araba Taylor <araba.taylor20@gmail.com>
2017-10-06 00:28:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@ def roll_call_dwarves(dwarves)# code an argument here
 end
 
 def summon_captain_planet(veggies)
-  veggies.collect! {|w| w.capitalize}
+  veggies.collect! {|w| w.capitalize + "!"}
 end
 
 def long_planeteer_calls# code an argument here
