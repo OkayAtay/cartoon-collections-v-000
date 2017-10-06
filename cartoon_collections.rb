66d3067423baaea76023441b5ec634e25bfedcb4
Araba Taylor <araba.taylor20@gmail.com>
2017-10-06 01:24:24 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,9 +16,6 @@ end
 
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
-   array.include?(cheese_types)
+   array.detect {|f| f.include?("cheddar", "gouda", "camembert")}
 end
 
-array = ["banana", "cheddar", "sock"]
-
-find_the_cheese(array)
